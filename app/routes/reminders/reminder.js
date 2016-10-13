1bd51e8355ccf93b08a7deb5f570ac909a8e1434
swanie21 <kswanie21@gmail.com>
2016-10-13 15:40:10 -0600

    routing to form correctly

--- app/routes/reminders/reminder.js
+++ app/routes/reminders/reminder.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('reminder', params.reminder_id);
+    return this.get('store').findRecord('reminder', params.reminder_id);
   }
 });
