8911afddf270885f22eb7d102ef3c5959a093f96
mjvalade <mjvalade@gmail.com>
2016-10-13 09:20:45 -0600

    implement link for individual reminder

--- app/router.js
+++ app/router.js
@@ -7,10 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('reminders', {
-    path: '/'
+  this.resource('reminders', function() {
+    this.resource('reminder', { path: 'reminder_id' });
   });
-  this.route('reminder');
 });
 
 export default Router;
--- app/routes/reminder.js
+++ app/routes/reminder.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model(params) {
-    return this.store.find('reminder', params.artist_id);
-  }
-});
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   model(params) {
+//     return this.store.find('reminder', params.reminder_id);
+//   }
+// });
