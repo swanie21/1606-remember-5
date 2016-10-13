e55a791e5b6b82264550bd9fbc6d0d3327fd71ac
swanie21 <kswanie21@gmail.com>
2016-10-13 16:20:56 -0600

    able to render new reminder onto page

--- app/controllers/reminders/add-reminder.js
+++ app/controllers/reminders/add-reminder.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  title: '',
+  date: '',
+  notes: '',
+
   actions: {
     createReminder() {
-      console.log('YES!!!!')
+      const reminder = this.getProperties('title', 'date', 'notes');
+      reminder.date = new Date(reminder.date);
+      this.get('store').createRecord('reminder', reminder).save().then(() => {
+        this.setProperties({ title: '', date: '', notes: '' });
+      });
     }
   }
 });
