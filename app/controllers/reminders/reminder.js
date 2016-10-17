2061933cd8e14894148d95b67a55e91ccc8ea8b7
swanie21 <kswanie21@gmail.com>
2016-10-17 17:37:21 -0600

    remove comments

--- app/controllers/reminders/reminder.js
+++ app/controllers/reminders/reminder.js
@@ -3,12 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     toggleEditing() {
-      console.log('clicked');
       this.toggleProperty('isEditing', true);
     },
     update() {
-      // let reminder = this.get('model').getProperties('date');
-      // reminder.date = new Date(reminder.date);
       this.get('model').save().then(() => {
         this.setProperties({ title: '', date: '', notes: '' });
         });
--- app/transforms/date.js
+++ app/transforms/date.js
@@ -1,11 +1,14 @@
 import DS from 'ember-data';
+import moment from 'moment';
 
 export default DS.Transform.extend({
   deserialize(serialized) {
+    if (serialized){return moment(serialized).format("dddd, MMM Do YYYY");}
     return serialized;
   },
 
-  serialize(deserialized) {
+  serialize(deserialized){
+    if(deserialized){return moment(deserialized).toISOString();}
     return deserialized;
   }
 });
