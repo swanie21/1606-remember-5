10260c3db3defd3380bbe494f300382d49fe22b4
swanie21 <kswanie21@gmail.com>
2016-10-17 17:38:41 -0600

    format date

--- app/transforms/date.js
+++ app/transforms/date.js
@@ -3,12 +3,16 @@ import moment from 'moment';
 
 export default DS.Transform.extend({
   deserialize(serialized) {
-    if (serialized){return moment(serialized).format("dddd, MMM Do YYYY");}
+    if (serialized) {
+      return moment(serialized).format("dddd, MMM Do YYYY");
+    }
     return serialized;
   },
 
-  serialize(deserialized){
-    if(deserialized){return moment(deserialized).toISOString();}
+  serialize(deserialized) {
+    if(deserialized) {
+      return moment(deserialized).toISOString();
+    }
     return deserialized;
   }
 });
